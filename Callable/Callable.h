b46a6f81989bd111dba655f254251c00d943e755
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-03-23 19:53:43 +0900

    Initial Commit

---
+++ Callable/Callable.h
@@ -0,0 +1,19 @@
+//
+//  Callable.h
+//  Callable
+//
+//  Created by kensuke-hoshikawa on 2018/03/23.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Callable.
+FOUNDATION_EXPORT double CallableVersionNumber;
+
+//! Project version string for Callable.
+FOUNDATION_EXPORT const unsigned char CallableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Callable/PublicHeader.h>
+
+
