3b6e75f135e95611a61f1684b9dd22bf63a9c3b0
sgr-ksmt <suguru-kishimoto@cookpad.com>
2018-04-02 16:47:32 +0900

    modified document.

--- Callable/Callable.swift
+++ Callable/Callable.swift
@@ -11,23 +11,24 @@ import Result
 import FirebaseFunctions
 
 public enum CallableError: Error {
-    /// server threw an error or if the resulting promise was rejected.
+    /// Server threw an error or if the resulting promise was rejected.
     case function(Error)
-    /// decode failed
+    /// Decode failed
     case decode(Error)
-    /// both result and error exist, or nil
+    /// Both result and error exist, or nil
     case illegalCombination(HTTPSCallableResult?, Error?)
 }
 
 public protocol Callable {
-    /// callable response that have to extend Decodable
+    /// Callable response that have to extend Decodable
     associatedtype Response: Decodable
 
-    /// name The name of the Callable HTTPS trigger.
+    /// Name The name of the Callable HTTPS trigger.
     var path: String { get }
-    /// data Parameters to pass to the trigger. Default is nil.
+    /// Data Parameters to pass to the trigger. Default is nil.
     var parameter: [String: Any]? { get }
 
+    /// Decoder for HTTPSCallableResult. Default is `JSONDecoder()`.
     var jsonDecoder: JSONDecoder { get }
 
     /// Call Callable HTTPS trigger asynchronously.
@@ -47,6 +48,7 @@ public extension Callable {
 
     public func call(completion: @escaping (Result<Response, CallableError>) -> Void) {
         let _jsonDecoder = jsonDecoder
+
         Functions.functions().httpsCallable(path)
             .call(parameter) { result, error in
                 switch (result, error) {
