6c67be9168ea7a55b94e741737c73386eda0e6c9
sgr-ksmt <suguru-kishimoto@cookpad.com>
2018-05-02 10:48:23 +0900

    implemented test case using mock session.

--- CallableTests/CallableTests.swift
+++ CallableTests/CallableTests.swift
@@ -9,28 +9,61 @@
 import XCTest
 @testable import Callable
 
+private func makeCallableError(details: [String: Any] = [:], code: Int = 0, description: String = "") -> NSError {
+    return NSError(
+        domain: "com.firebase.functions",
+        code: code,
+        userInfo: ["details": details, NSLocalizedDescriptionKey: description])
+}
+
 class CallableTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    class MockSession: Session {
+        let data: Data?
+        let error: Error?
+        init(data: Data? = nil, error: Error? = nil) {
+            self.data = data
+            self.error = error
+        }
+
+        func send(_ path: String, parameter: [String : Any]?, handler: @escaping (Data?, Error?) -> Void) {
+            handler(data, error)
         }
     }
-    
+
+    struct TestRequest: Callable {
+        struct Response: Decodable {
+            let name: String
+        }
+        let path: String = "httpcallabletest"
+    }
+
+    func testSuccessMockSession() {
+        let json = try! JSONSerialization.data(withJSONObject: ["name": "Mike"], options: [])
+        let expectation = XCTestExpectation()
+        TestRequest().call(MockSession(data: json)) { result in
+            XCTAssertNotNil(result.value)
+            XCTAssertNil(result.error)
+            XCTAssertEqual(result.value?.name, "Mike")
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+    }
+
+    func testFailureMockSession() {
+        let error = makeCallableError(details: ["code": "foobar"])
+        let expectation = XCTestExpectation()
+        TestRequest().call(MockSession(error: error)) { result in
+            XCTAssertNotNil(result.error)
+            XCTAssertNil(result.value)
+            if let error = result.error, case let CallableError.function(functionError as NSError) = error {
+                XCTAssertNotNil(functionError.userInfo)
+                XCTAssertNotNil(functionError.userInfo["details"] as? [String: Any])
+                XCTAssertEqual((functionError.userInfo["details"] as? [String: Any])?["code"] as? String, "foobar")
+            } else {
+                XCTFail()
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1.0)
+    }
 }
